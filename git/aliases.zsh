b6734372d9345466bb56c31f6b70fd9cb6378465
Tom Brown <tom@joingrouper.com>
2012-12-20 12:09:09 -0500

    Fixed aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,8 +6,11 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
+# Tom's Aliases
 alias g="git commit -am "
+alias gpp='git pull && git push'
+
+# The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
