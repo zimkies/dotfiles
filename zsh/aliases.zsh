25817b47d3a5fbf2b62a3ce4770e449a621b27cf
Tom Brown <tom@joingrouper.com>
2013-02-05 18:12:32 -0500

    Added dbup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,7 +6,7 @@ alias anno='bundle exec annotate -p before'
 
 alias dbg='rake db:test:prepare && bundle exec guard'
 
-alias dbr='curl -o latest.dump `heroku pgbackups:url --app grouper-app` && pg_restore --verbose --clean --no-acl --no-owner -U grouper -d grouper_app_development latest.dump'
+alias dbup='curl -o latest.dump `heroku pgbackups:url --app grouper-app` && pg_restore --verbose --clean --no-acl --no-owner -U grouper -d grouper_app_development latest.dump'
 
 alias update-staging='heroku pgbackups:restore --app grouper-staging HEROKU_POSTGRESQL_CYAN  `heroku pgbackups:url --app grouper-app` --confirm grouper-staging'
 
