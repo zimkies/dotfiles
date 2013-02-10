ce3fe943d62d30161fc670a050a6962609011a7c
Tom Brown <tom@joingrouper.com>
2013-02-10 15:55:49 -0500

    Added VirtualEnvWrapper setup

---
+++ python/aliases.zsh
@@ -0,0 +1,6 @@
+
+# VirtualEnvWrapper initialization
+# http://virtualenvwrapper.readthedocs.org/en/latest/install.html
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
