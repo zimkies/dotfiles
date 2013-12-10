de21454c0b3d73a73973c389ce2d31b72628498c
Benjamin Kies <ben@joingrouper.com>
2013-12-10 12:33:17 -0500

    fixed trim trailing white space misspelling, 80char length

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -2,4 +2,6 @@
 rbenv rehash 2>/dev/null
 
 export RBENV_ROOT=/usr/local/var/rbenv
-if which rbenv > /dev/null; then eval "$(rbenv init - zsh)"; fi
+# export PATH="$HOME/.rbenv/bin:$PATH"
+# if which rbenv > /dev/null; then eval "$(rbenv init - zsh)"; fi
+eval "$(rbenv init - zsh)"
