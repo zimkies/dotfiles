07d05628ef916e915c8a91df2fca7906a65f2eee
Benjamin Kies <ben@joingrouper.com>
2014-02-06 17:08:56 -0500

    Fixed rbenv to work properly
    For some reason it was causing irb to hang on my system

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,7 +1,5 @@
 # rehash shims
 rbenv rehash 2>/dev/null
 
-export RBENV_ROOT=/usr/local/var/rbenv
-# export PATH="$HOME/.rbenv/bin:$PATH"
-# if which rbenv > /dev/null; then eval "$(rbenv init - zsh)"; fi
-eval "$(rbenv init - zsh)"
+export PATH="$HOME/.rbenv/bin:$PATH"
+if which rbenv > /dev/null; then eval "$(rbenv init - zsh)"; fi
